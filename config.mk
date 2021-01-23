d2f38df6a2269ea58017f48abd1ba04b187cef9e
mamemomonga <mamemomonga@gmail.com>
2021-01-24 08:38:49 +0900

    jtag2updi設定

--- config.mk
+++ config.mk
@@ -7,7 +7,10 @@ AVR_TOOLCHAINS=avr8-gnu-toolchain-osx-3.6.2.514-darwin.any.x86_64.tar.gz
 # Microchip Packs Repository
 # ------------------------------------------
 DFP_ATMEGA=Atmel.ATmega_DFP.1.6.364
+
 DFP_ATTINY=Atmel.ATtiny_DFP.1.8.332
+# DFP_ATTINY=Atmel.ATtiny_DFP.1.6.326
+# DFP_ATTINY=Atmel.ATtiny_DFP.1.3.229
 
 # ------------------------------------------
 # AVRDUDE
